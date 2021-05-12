b2018b043feae0c26de0a91bcef9b403ce00fa79
Naniduan <41004075+Naniduan@users.noreply.github.com>
2021-05-13 00:09:20 +0300

    Louvain algorithm. The only thing missing is the Louvain algorithm itself

--- src/main/kotlin/graph.kt
+++ src/main/kotlin/graph.kt
@@ -1,42 +1,41 @@
-import java.util.Random
-val random = Random()
-fun rand(from: Double, to: Double) : Double { return random.nextInt((to*1000).toInt()-(from*1000).toInt())/1000.0 + from }
+class Node{
+    public var vertexes: MutableSet<Vertex> = mutableSetOf()
 
-val graphSize: Double = 1000.0
+    //public var color
+    //public var x
+    //public var y
 
-open class Node{
-    public var neighbors: Set<Node> = emptySet()
-    public var color = "ffffff"
-    public var x: Double = rand(-graphSize,graphSize)
-    public var y: Double = rand(-graphSize,graphSize)
-    public var size: Double = 1.0
+    public fun neighbours(): Set<Node>{
+        val neighbours: MutableSet<Node> = mutableSetOf()
+        for (vertex in this.vertexes){
+            for (node in vertex.nodes){
+                if (node!=this) neighbours.add(node)
+            }
+        }
+        return neighbours
+    }
 
-    public fun adjust(node: Node){
-        neighbors += node
-        node.neighbors += this
+    public fun sumOfWeights(): Double{
+        var sum: Double = 0.0
+        for (vertex in this.vertexes){
+            sum+=vertex.weight
+        }
+        return sum
     }
 }
 
-open class Graph{
-    public var nodes: Set<Node> = emptySet()
+class Vertex(node1: Node, node2: Node, public var weight: Double){
+    public var nodes = setOf(node1, node2)
+}
 
-    public fun addNode(){
-        var node = Node()
-        nodes += node
-    }
+class Graph{
+    public var nodes: MutableSet<Node> = mutableSetOf()
+    public var vertexes: MutableSet<Vertex> = mutableSetOf()
 
-    public fun deleteNode(node: Node){
-        nodes -= node
-        for (i in node.neighbors){
-            i.neighbors -= node
-        }
-    }
-
-    public fun layoutGraph(){TODO()}
-
-    //public fun findKeyNodes(){TODO()}
-
-    public fun findCommunities(){
-        louvain(this)
+    public fun connect(node1: Node, node2: Node, weight: Double){
+        val vertex = Vertex(node1,node2,weight)
+        node1.vertexes.add(vertex)
+        node2.vertexes.add(vertex)
+        vertexes.add(vertex)
     }
 }
--- src/main/kotlin/louvain.kt
+++ src/main/kotlin/louvain.kt
@@ -2,18 +2,54 @@
 // which Graph.addNode doesn't. Graph doesn't override Community
 // because that would be nonsense
 
-class Comunity(){
-    public var nodes: Set<Node> = emptySet()
+class Community(node: Node){
+    public var nodes: MutableSet<Node> = mutableSetOf(node)
+    public var sumOfAllWeights: Double = 0.0
+
+    public fun sumOfNeighbouringWeights(node: Node): Double{
+        if (nodes.size <= 1) return 0.0
+
+        var theSum: Double = 0.0
+
+        for (vertex in node.vertexes){
+            for (i in vertex.nodes){
+                if (i!=node && i in nodes) theSum += vertex.weight
+            }
+        }
+
+        return theSum
+    }
 
     public fun addNode(node: Node){
-        nodes += node
+        nodes.add(node)
+        sumOfAllWeights += sumOfNeighbouringWeights(node)
     }
 
     public fun deleteNode(node: Node){
-        nodes -= node
+        nodes.remove(node)
+        sumOfAllWeights -= sumOfNeighbouringWeights(node)
     }
 }
 
+fun sumOfWeights(vertexes: MutableSet<Vertex>): Double{
+    var theSum: Double = 0.0
+
+    for (vertex in vertexes) theSum+=vertex.weight
+
+    return theSum
+}
+
 fun louvain(graph: Graph){
-    TODO()
+    var communities: MutableSet<Community> = mutableSetOf()
+    val sumOfWeightsInGraph: Double = sumOfWeights(graph.vertexes)
+
+    for (i in graph.nodes){
+        communities.add(Community(i))
+    }
+
+    var modularity: Double = 0.0
+
+    fun firstPhase(){
+        
+    }
 }
